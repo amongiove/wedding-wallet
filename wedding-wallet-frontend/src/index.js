2c3c0623175cdd0eeb2928f96516e379d620d47e
amongiove <amandamongiove@gmail.com>
2021-07-26 15:28:47 -0500

    add edit/delete button icons

--- wedding-wallet-frontend/src/index.js
+++ wedding-wallet-frontend/src/index.js
@@ -294,15 +294,17 @@ function displayCategories(){
         let table = document.createElement('table');
         id = category.replace(/[^A-Z0-9]/ig, "")
         table.id = `${id}`
-        table.classList.add("table", "table-hover", "table-danger", "table-striped");
+        table.classList.add("table", "table-striped", "table-hover")
         table.style = "width:100%"
         table.innerHTML = `
             <caption class="caption-top"><b><i>${category}</b></i></caption>
-            <tr>
-                <th style="width:35%">Expense Item</th>
-                <th style="width:35%">Expense Cost</th>
-                <th style="width:20%"></th>
-            </tr
+            <thead>
+                <tr class="table table-danger table-sm head-row">
+                    <th class="fw-light" style="width:35%">Expense Item</th>
+                    <th class="fw-light" style="width:35%">Expense Cost</th>
+                    <th class="fw-light" style="width:20%"></th>
+                </tr>
+            </thead>
         `
         categoryList.appendChild(table);
     });        
@@ -341,11 +343,15 @@ function getExpenses(){
                 expenseName = expenseRow.insertCell(0)
                 expenseName.innerHTML =  `${expense.name}`
                 expenseAmount = expenseRow.insertCell(1)
-                expenseAmount.innerHTML =  `${expense.amount}`
+                expenseAmount.innerHTML =  `$${expense.amount}`
                 expenseButtons = expenseRow.insertCell(2)
                 expenseButtons.innerHTML =  `
-                    <button class="edit-expense" data-id="${expense.id}" type="button"onclick="document.getElementById('edit-expense-form').style.display='block'; updateExpense(${expense.id})">Edit</button> 
-                    <button class="delete-expense" data-id="${expense.id}" type="button" onclick="return confirm('Are you sure you want to delete?')?deleteExpense(${expense.id}):''">Delete</button>
+                    <button class="edit-expense" data-id="${expense.id}" type="button"onclick="document.getElementById('edit-expense-form').style.display='block'; updateExpense(${expense.id})"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+                    <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+                  </svg></button> 
+                    <button class="delete-expense" data-id="${expense.id}" type="button" onclick="return confirm('Are you sure you want to delete?')?deleteExpense(${expense.id}):''"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                    <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                  </svg></button>
                 `
             })
         })
@@ -392,11 +398,15 @@ function displayExpense(category, name, amount, notes, id){
     expenseName = expenseRow.insertCell(0)
     expenseName.innerHTML =  `${name}`
     expenseAmount = expenseRow.insertCell(1)
-    expenseAmount.innerHTML =  `${amount}`
+    expenseAmount.innerHTML =  `$${amount}`
     expenseButtons = expenseRow.insertCell(2)
     expenseButtons.innerHTML =  `
-        <button class="edit-expense" data-id="${id}" type="button"onclick="document.getElementById('edit-expense-form').style.display='block'; updateExpense(${id})">Edit</button> 
-        <button class="delete-expense" data-id="${id}" type="button" onclick="return confirm('Are you sure you want to delete?')?deleteExpense(${id}):''">Delete</button>
+        <button class="edit-expense" data-id="${id}" type="button"onclick="document.getElementById('edit-expense-form').style.display='block'; updateExpense(${id})"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+        <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+      </svg></button> 
+        <button class="delete-expense" data-id="${id}" type="button" onclick="return confirm('Are you sure you want to delete?')?deleteExpense(${id}):''"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+        <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+      </svg></button>
         `
     totalExpense();
     showBalance();
