fca6bc8f20ea372e0a123724ef9a7b8621ac7b73
amongiove <amandamongiove@gmail.com>
2021-07-27 14:23:28 -0500

    Fix category async load issue and add css

--- wedding-wallet-frontend/src/index.js
+++ wedding-wallet-frontend/src/index.js
@@ -233,8 +233,9 @@ function displayBudget(){
     budgetDisplay.removeAttribute("hidden");
     const logOutBtn = document.querySelector("#logout-btn");
     logOutBtn.removeAttribute("hidden");
-    displayCategories();
-    getExpenses();
+    Promise.resolve(displayCategories())
+    .then(getExpenses());
+    // getExpenses();
     getUserData().then((user) => {
         budget = user.data.attributes.budget.amount
         budgetAmount.textContent = budget;
@@ -280,8 +281,7 @@ function editBudgetFetch(newAmount){
 
 //categories
 function getCategories(){
-    let categories = []
-    listCategories = fetch('http://localhost:3000/api/v1/categories', {
+    return fetch('http://localhost:3000/api/v1/categories', {
         method: 'GET',
         headers: {
         Authorization: `Bearer ${localStorage.getItem('jwt_token')}`
@@ -289,20 +289,21 @@ function getCategories(){
     })
     .then(response => response.json())
     .then(json => {return json.category.data})
-    
-    listCategories.then((list) => {
+    .then((list) => {
+        let localCategories = []
         list.forEach(category => {
-            categories.push(category.attributes.name)
+            localCategories.push(category.attributes.name)
         })
-        return categories
-    })  
-    return categories
+        return localCategories
+    })
 }
 
 function displayCategories(){
     console.log("display")
     categoryList = document.querySelector('#expense-list-categories');
-    categories.forEach( (category) => {
+    // return Promise.resolve(getCategories())
+    return Promise.resolve(categories)
+    .then(categories => categories.forEach( (category) => {
         id = category.replace(/[^A-Z0-9]/ig, "")
         let accordian = document.createElement('div');
         accordian.classList.add('accoridan-item');
@@ -326,7 +327,7 @@ function displayCategories(){
                 </div>
             </div>`
         categoryList.appendChild(accordian);
-    });        
+    }));        
 }
 
 //potential for dynamically coding categories into form
@@ -345,35 +346,34 @@ function getExpenses(){
     getUser.then((user) => {
         expenses = user.data.attributes.expenses
         expenses.forEach (expense => {
-            category = fetch(`http://localhost:3000/api/v1/categories/${expense.category_id}`, {
+            fetch(`http://localhost:3000/api/v1/categories/${expense.category_id}`, {
                     method: 'GET',
                     headers: {
                     Authorization: `Bearer ${localStorage.getItem('jwt_token')}`
                     }
                 })
                 .then(response => response.json())
-                .then(json => { return json.category.data.attributes.name
+                .then(json => { return json.category.data.attributes.name})
+                .then(categoryName => {
+                    const id = categoryName.replace(/[^A-Z0-9]/ig, "")
+                    table = document.querySelector(`#${id}`)
+                    expenseRow = table.insertRow(-1);
+                    expenseRow.id = `expense-${expense.id}`
+                    expenseName = expenseRow.insertCell(0)
+                    expenseName.innerHTML =  `${expense.name}`
+                    expenseAmount = expenseRow.insertCell(1)
+                    expenseAmount.innerHTML =  `$${expense.amount}`
+                    expenseButtons = expenseRow.insertCell(2)
+                    expenseButtons.style = "text-align: right"
+                    expenseButtons.innerHTML =  `
+                        <button class="btn btn-outline-secondary edit-expense" data-id="${expense.id}" data-bs-toggle="modal" data-bs-target="#edit-expense-form"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
+                        <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
+                      </svg></button> 
+                        <button class="btn btn-outline-secondary delete-expense" data-id="${expense.id}" type="button" onclick="return confirm('Are you sure you want to delete?')?deleteExpense(${expense.id}):''"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
+                        <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
+                      </svg></button>
+                    `
                 })
-            category.then((categoryName) => {
-                const id = categoryName.replace(/[^A-Z0-9]/ig, "")
-                table = document.querySelector(`#${id}`)
-                expenseRow = table.insertRow(-1);
-                expenseRow.id = `expense-${expense.id}`
-                expenseName = expenseRow.insertCell(0)
-                expenseName.innerHTML =  `${expense.name}`
-                expenseAmount = expenseRow.insertCell(1)
-                expenseAmount.innerHTML =  `$${expense.amount}`
-                expenseButtons = expenseRow.insertCell(2)
-                expenseButtons.style = "text-align: right"
-                expenseButtons.innerHTML =  `
-                    <button class="btn btn-outline-secondary edit-expense" data-id="${expense.id}" data-bs-toggle="modal" data-bs-target="#edit-expense-form"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pencil-fill" viewBox="0 0 16 16">
-                    <path d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"/>
-                  </svg></button> 
-                    <button class="btn btn-outline-secondary delete-expense" data-id="${expense.id}" type="button" onclick="return confirm('Are you sure you want to delete?')?deleteExpense(${expense.id}):''"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash-fill" viewBox="0 0 16 16">
-                    <path d="M2.5 1a1 1 0 0 0-1 1v1a1 1 0 0 0 1 1H3v9a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V4h.5a1 1 0 0 0 1-1V2a1 1 0 0 0-1-1H10a1 1 0 0 0-1-1H7a1 1 0 0 0-1 1H2.5zm3 4a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 .5-.5zM8 5a.5.5 0 0 1 .5.5v7a.5.5 0 0 1-1 0v-7A.5.5 0 0 1 8 5zm3 .5v7a.5.5 0 0 1-1 0v-7a.5.5 0 0 1 1 0z"/>
-                  </svg></button>
-                `
-            })
         })
     })
 }
